f1edc4670817523645bc0b86fc925a721c538333
Jack <wangdeshui@outlook.com>
2016-08-06 10:35:18 +0800

    init

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>This is the Hello App Component</h1>
+            </div>
+        )
+    }
+
+}
+
+export default App
